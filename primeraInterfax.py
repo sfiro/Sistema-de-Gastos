7e37dc4907520580e8f888228c9d0b95655aa8c6
Debbie Arredondo <chevi6@gmail.com>
2022-12-22 05:45:12 -0500

    se crea agrega el scrollbar para el listbox, tambien se empiezan a traer los datos de la base de datos y desplegarlo en el listbox, se agrega la funcionalidad de actualizar y de agregar

--- primeraInterfax.py
+++ primeraInterfax.py
@@ -1,13 +1,31 @@
 import tkinter as tk
-
+import mysql.connector
 
 class interfax(tk.Tk):
     def __init__(self):
         super().__init__()
+
+#----------- conexion a la base de datos ------------------------
+        self.connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='3231213a',
+            db='INGENIO'
+        )
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("SELECT database();")
+        registro = self.cursor.fetchone()
+
+        print("conexion establecida exitosamente")
+        print("la base de datos se llama:",registro)
+
         
+
+# ------------- creación de la ventana grafica ------------------
+
         self.title("Vendedores")
         self.geometry("400x300")
-        self.columnconfigure((0,1,2),weight=1)
+        self.columnconfigure((0,1),weight=1)
 
         self.rowconfigure((0,1,2,3,4),weight=0)
 
@@ -35,15 +53,45 @@ class interfax(tk.Tk):
         self.eliminarButton.grid(row=3,column=2,padx=10,pady=10)
 
 #----------------List BOX  -----------------------------------------------------   
-        self.listaList = tk.Listbox(master=self)
-        self.listaList.grid(row = 4, column = 0, columnspan = 3, sticky="nsew")
+        self.lista = tk.Listbox(master=self)
+        self.lista.grid(row = 4, column = 0, columnspan = 3, sticky="nsew")
+        self.scrollbar = tk.Scrollbar(master=self,orient="vertical" ,command=self.lista.yview)
+        self.lista.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.grid(row = 4, column = 2,sticky="nse")
 
-    def ingresar():
-        pass
-    def actualizar():
-        pass
+        self.actualizar()
+
+    def ingresar(self):
+        #print("hola")
+        name = self.entradaNombre.get()
+        email = self.entradaCorreo.get()
+        phone = self.entradaTelefono.get()
+
+        
+        self.cursor.execute("INSERT INTO Vendedores(vendedor,correo,telefono) VALUES('{}','{}','{}');".format(name,email,phone))
+        self.connection.commit()
+        print(name + email + phone)
+        
+
+        self.entradaNombre.delete(0,tk.END)
+        self.entradaTelefono.delete(0,tk.END)
+        self.entradaCorreo.delete(0,tk.END)
+        self.actualizar()
+
     def eliminar():
         pass
+    def actualizar(self):
+        
+        sql = "SELECT * FROM Vendedores"
+        self.cursor.execute(sql)
+        n=0
+        for dato in self.cursor.fetchall():
+            # print(dato)
+            # print(list(dato[1:]))
+            self.lista.insert(n,list(dato[1:]))
+            n = n+1
+        #self.connection.commit()
+        
 
 if __name__ == "__main__":
     ventana = interfax()
