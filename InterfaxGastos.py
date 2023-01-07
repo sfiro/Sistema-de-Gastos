05cac1caf6e681cab6a67dc7e85cf56cdb40a4f1
Debbie Arredondo <chevi6@gmail.com>
2023-01-07 05:58:13 -0500

    se hace toda la interfax para agregar proveedores y de añade la conexión a la base de datos

--- InterfaxGastos.py
+++ InterfaxGastos.py
@@ -4,83 +4,6 @@ import mysql.connector
 
 
 
-# -------------------- Ventana secundaria del modulo de gasto (agregar gasto)---------------------------------------
-class AgregarGastos(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Agregar Gasto")
-        self.geometry("500x300")
-
-        self.ingresarButton = tk.Button(master=self,text="Guardar",command=self.guardar)
-        self.ingresarButton.grid(row=0,column=0,padx=10,pady=10)
-        self.actualizarButton = tk.Button(master=self,text="Eliminar",command=self.eliminar)
-        self.actualizarButton.grid(row=0,column=1,padx=10,pady=10)
-    
-    def guardar(self):
-        pass
-
-    def eliminar(self):
-        pass
-
-# -------------------- Ventana secundaria del modulo de gasto (agregar proveedor)---------------------------------------
-class AgregarProveedor(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Agregar Proveedor")
-        self.geometry("500x300")
-
-        self.frame1 = tk.Frame(master=self)
-        self.frame1.grid(row=0,column=0,sticky="nsew")
-
-        self.frame2 = tk.Frame(master=self)
-        self.frame2.grid(row=1,column=0,sticky="nsew")
-
-# ------------------ label de la ventana proveedores -------------------------------------
-
-        self.empresa = tk.Label(master = self.frame1, text = "Empresa:")
-        self.empresa.grid(row = 0, column = 0, padx = 10, pady = 10)
-
-        self.representante = tk.Label(master = self.frame1, text = "Representante legal:")
-        self.representante.grid(row = 1, column = 0, padx = 10, pady = 10)
-
-        self.nit = tk.Label(master = self.frame1, text = "NIT:")
-        self.nit.grid(row = 2, column = 0, padx = 10, pady = 10)
-
-        self.celular = tk.Label(master = self.frame1, text = "Celular:")
-        self.celular.grid(row = 3, column = 0, padx = 10, pady = 10)
-
-# ------------------ Entry de la ventana proveedores -------------------------------------
-
-        self.entradaEmpresa = tk.Entry(master = self.frame1, textvariable="Empresa:")
-        self.entradaEmpresa.grid(row = 0, column = 1, padx = 10, pady = 10)
-
-        self.entradaRepresentante = tk.Entry(master = self.frame1, textvariable="Representante legal:")
-        self.entradaEmpresa.grid(row = 1, column = 1, padx = 10, pady = 10)
-
-        self.entradaNIT = tk.Entry(master = self.frame1, textvariable="NIT:")
-        self.entradaNIT.grid(row = 2, column = 1, padx = 10, pady = 10)
-
-        self.entradaTelefono = tk.Entry(master = self.frame1, textvariable="Telefono:")
-        self.entradaTelefono.grid(row = 3, column = 1, padx = 10, pady = 10)
-
-        # self.guardarButton = tk.Button(master=self.frame1,text="Guardar",command=self.guardar)
-        # self.guardarButton.grid(row=0,column=0,padx=10,pady=10)
-        # self.actualizarButton = tk.Button(master=self.frame1,text="Actualizar",command=self.actualizar)
-        # self.actualizarButton.grid(row=0,column=1,padx=10,pady=10)
-        # self.eliminarButton = tk.Button(master=self.frame1,text="Eliminar",command=self.eliminar)
-        # self.eliminarButton.grid(row=0,column=2,padx=10,pady=10)
-    
-    def guardar(self):
-        pass
-
-    def eliminar(self):
-        pass
-
-    def actualizar(self):
-        pass
-
 # -------------------- Ventana Principal del modulo de gasto ---------------------------------------
 class SeguimientoGastos(tk.Tk):
     def __init__(self):
@@ -101,6 +24,181 @@ class SeguimientoGastos(tk.Tk):
     def proveedor(self):
         proveedorIngreso = AgregarProveedor()
         proveedorIngreso.mainloop()
+
+
+# -------------------- Ventana secundaria del modulo de gasto (agregar gasto)---------------------------------------
+class AgregarGastos(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Agregar Gasto")
+        self.geometry("500x300")
+
+        self.ingresarButton = tk.Button(master=self,text="Guardar",command=self.guardar)
+        self.ingresarButton.grid(row=0,column=0,padx=10,pady=10)
+        self.actualizarButton = tk.Button(master=self,text="Eliminar",command=self.eliminar)
+        self.actualizarButton.grid(row=0,column=1,padx=10,pady=10)
+
+    
+    def guardar(self):
+        pass
+
+    def eliminar(self):
+        pass
+
+# -------------------- Ventana secundaria del modulo de gasto (agregar proveedor)---------------------------------------
+class AgregarProveedor(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        #----------- conexion a la base de datos ------------------------
+        self.connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='3231213a',
+            db='INGENIO'
+        )
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("SELECT database();")
+        registro = self.cursor.fetchone()
+
+        print("conexion establecida exitosamente")
+        print("la base de datos se llama:",registro)
+
+        self.title("Agregar Proveedor")
+        self.geometry("1200x900")
+
+        self.columnconfigure(0, weight=1, minsize=70)
+
+        self.frame1 = tk.Frame(master=self)
+        self.frame1.grid(row=0,column=0,sticky="nw")
+        
+
+        self.frame2 = tk.Frame(master=self)
+        self.frame2.grid(row=0,column=1,sticky="nw")
+
+        self.frame3 = tk.Frame(master=self)
+        self.frame3.grid(row=1,column=0,columnspan=2)
+
+# ------------------ label de la ventana proveedores -------------------------------------
+
+        self.empresa = tk.Label(master = self.frame1, text = "Empresa:")
+        self.empresa.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        self.representante = tk.Label(master = self.frame1, text = "Representante legal:")
+        self.representante.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+        self.nit = tk.Label(master = self.frame1, text = "NIT:")
+        self.nit.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+        self.celular = tk.Label(master = self.frame1, text = "Celular:")
+        self.celular.grid(row = 3, column = 0, padx = 10, pady = 10)
+
+        self.pyme = tk.Label(master = self.frame1, text = "Es PYME:")
+        self.pyme.grid(row = 4, column = 0, padx = 10, pady = 10)
+
+        self.departamento = tk.Label(master = self.frame1, text = "Departamento:")
+        self.departamento.grid(row = 5, column = 0, padx = 10, pady = 10)
+
+        self.municipio = tk.Label(master = self.frame1, text = "Municipio:")
+        self.municipio.grid(row = 6, column = 0, padx = 10, pady = 10)
+
+        self.direccion= tk.Label(master = self.frame1, text = "Direccion:")
+        self.direccion.grid(row = 7, column = 0, padx = 10, pady = 10)
+
+        self.categoria = tk.Label(master = self.frame1, text = "Categoria:")
+        self.categoria.grid(row = 8, column = 0, padx = 10, pady = 10)
+
+        self.descripcion = tk.Label(master = self.frame1, text = "Descripcion:")
+        self.descripcion.grid(row = 9, column = 0, padx = 10, pady = 10)
+
+        self.rutButton = tk.Button(master=self.frame1,text="Adjuntar RUT",command=self.guardar)
+        self.rutButton.grid(row=10,column=0,padx=10,pady=10)
+
+# ------------------ Entry de la ventana proveedores -------------------------------------
+
+        self.entradaEmpresa = tk.Entry(master = self.frame1, textvariable="Empresa:")
+        self.entradaEmpresa.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+        self.entradaRepresentante = tk.Entry(master = self.frame1, textvariable="Representante:")
+        self.entradaRepresentante.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+        self.entradaNIT = tk.Entry(master = self.frame1, textvariable="NIT:")
+        self.entradaNIT.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+        self.entradaTelefono = tk.Entry(master = self.frame1, textvariable="Telefono:")
+        self.entradaTelefono.grid(row = 3, column = 1, padx = 10, pady = 10)
+
+        self.comboBoxPyme = ttk.Combobox(master = self.frame1)
+        self.comboBoxPyme['values'] = ("SI","NO")
+        self.comboBoxPyme.grid(row = 4, column = 1, padx = 10, pady = 10)
+
+        self.comboBoxDepartamento = ttk.Combobox(master = self.frame1)
+        self.comboBoxDepartamento['values'] = ("Risaralda","Valle")
+        self.comboBoxDepartamento.grid(row = 5, column = 1, padx = 10, pady = 10)
+
+        self.comboBoxMunicipio = ttk.Combobox(master = self.frame1)
+        self.comboBoxMunicipio['values'] = ("Cali","Pereira")
+        self.comboBoxMunicipio.grid(row = 6, column = 1, padx = 10, pady = 10)
+
+        self.direccion = tk.Entry(master = self.frame1, text = "Direccion:")
+        self.direccion.grid(row = 7, column = 1, padx = 10, pady = 10)
+
+        self.comboBoxCategoria = ttk.Combobox(master = self.frame1)
+        self.comboBoxCategoria['values'] = ("Madera","Impresiones")
+        self.comboBoxCategoria.grid(row = 8, column = 1, padx = 10, pady = 10)
+
+        self.descripcion = tk.Entry(master = self.frame1, text = "descripcion:")
+        self.descripcion.grid(row = 9, column = 1, padx = 10, pady = 10)
+
+        self.labelRut = tk.Label(master = self.frame1, text = "Vacio")
+        self.labelRut.grid(row = 10, column = 1, padx = 10, pady = 10)
+
+#----------------- frame 2 proveedores -------------------------------------------
+
+        self.guardarButton = tk.Button(master=self.frame2,text="Guardar",command=self.guardar)
+        self.guardarButton.grid(row=0,column=0,padx=10,pady=10)
+
+        self.actualizarButton = tk.Button(master=self.frame2,text="Actualizar",command=self.actualizar)
+        self.actualizarButton.grid(row=0,column=1,padx=10,pady=10)
+
+        self.eliminarButton = tk.Button(master=self.frame2,text="Eliminar",command=self.eliminar)
+        self.eliminarButton.grid(row=0,column=2,padx=10,pady=10)
+
+#-------------------frame 3 proveedores treeview ----------------------------------
+
+        self.treeview = ttk.Treeview(master=self.frame3,columns=('col1','col2','col3','col4','col5','col6','col7','col8','col9','col10','col11'))
+        
+        self.scrollbarTree = ttk.Scrollbar(master=self.frame3,orient="horizontal")
+        self.treeview.configure(xscrollcommand=self.scrollbarTree.set)
+        self.scrollbarTree.config(command=self.treeview.xview)
+        
+
+        self.treeview.heading('#0', text='ID')
+        self.treeview.column('#0', width=70)
+        self.treeview.heading('col1', text='Empresa')
+        self.treeview.heading('col2', text='Representante')
+        self.treeview.heading('col3', text='NIT')
+        self.treeview.heading('col4', text='Telefono')
+        self.treeview.heading('col5', text='Es PYME')
+        self.treeview.heading('col6', text='Departamento')
+        self.treeview.heading('col7', text='Municipio')
+        self.treeview.heading('col8', text='Direccion')
+        self.treeview.heading('col9', text='Categoria')
+        self.treeview.heading('col10', text='Descripcion')
+        self.treeview.heading('col11', text='RUT')
+        self.treeview.grid(row = 1, column = 0, columnspan = 3, sticky="nsew")
+        self.scrollbarTree.grid(row = 2, column = 0, sticky="ew")
+    
+    def guardar(self):
+        pass
+
+    def eliminar(self):
+        pass
+
+    def actualizar(self):
+        pass
+
 
 
 if __name__ == "__main__":
